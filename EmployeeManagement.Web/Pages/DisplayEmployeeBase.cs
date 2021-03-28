d51aa330a251d41c5eab8b9c728dc928ab551445
Ingvar Derešivski <inde999@gmail.com>
2021-03-28 13:39:54 +0300

    DisplayEmployeePartialView

---
+++ EmployeeManagement.Web/Pages/DisplayEmployeeBase.cs
@@ -0,0 +1,16 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Web.Pages
+{
+    public class DisplayEmployeeBase : ComponentBase
+    {
+        [Parameter]
+        public Employee Employee { get; set; }
+        public bool ShowFooter { get; set; }
+    }
+}
